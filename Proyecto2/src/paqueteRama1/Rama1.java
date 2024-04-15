dba498881250c38ed1bcdbef313de403261553b0
sergi <sergi@LAPTOP-AL9OB4NJ>
2024-04-15 18:34:51 +0200

    Segundo Commit

--- Proyecto2/src/paqueteRama1/Rama1.java
+++ Proyecto2/src/paqueteRama1/Rama1.java
@@ -5,6 +5,8 @@ public class Rama1 {
 	public static void main(String[] args) {
 		System.out.println("Este es el primer mensaje de la Rama1");
 		System.out.println("Este es el segundo mensaje de la Rama1");
+		System.out.println("Tercer mensaje de la Rama1");
+		System.out.println("Cuarto mensaje de la Rama1");
 	}
 
 }
