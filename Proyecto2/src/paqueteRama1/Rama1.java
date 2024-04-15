68270c2f0bb10608ac2b6e36955026a70304915b
sergi <sergi@LAPTOP-AL9OB4NJ>
2024-04-15 18:30:30 +0200

    Commit de la rama1

---
+++ Proyecto2/src/paqueteRama1/Rama1.java
@@ -0,0 +1,10 @@
+package paqueteRama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		System.out.println("Este es el primer mensaje de la Rama1");
+		System.out.println("Este es el segundo mensaje de la Rama1");
+	}
+
+}
